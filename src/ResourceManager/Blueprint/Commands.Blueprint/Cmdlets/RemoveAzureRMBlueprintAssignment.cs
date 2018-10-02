bf02ce459878536be4504fa364b9c7c37164119c
Jeff Bienstadt <jeffbi@jetstreamsoftware.com>
2018-10-02 15:07:44 -0700

    Add pipeline input capability to remove assignment cmdlet, including using PSBlueprintAssignment objects.

--- src/ResourceManager/Blueprint/Commands.Blueprint/Cmdlets/RemoveAzureRMBlueprintAssignment.cs
+++ src/ResourceManager/Blueprint/Commands.Blueprint/Cmdlets/RemoveAzureRMBlueprintAssignment.cs
@@ -12,6 +12,7 @@
 // limitations under the License.
 // ----------------------------------------------------------------------------------
 
+using Microsoft.Azure.Commands.Blueprint.Models;
 using Microsoft.Azure.Management.Blueprint;
 using System;
 using System.Collections.Generic;
@@ -22,15 +23,18 @@ using System.Threading.Tasks;
 
 namespace Microsoft.Azure.Commands.Blueprint.Cmdlets
 {
-    [Cmdlet(VerbsCommon.Remove, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "BlueprintAssignment")]
+    [Cmdlet(VerbsCommon.Remove, ResourceManager.Common.AzureRMConstants.AzureRMPrefix + "BlueprintAssignment", SupportsShouldProcess = true)]
     public class RemoveAzureRMBlueprintAssignment : BlueprintCmdletBase
     {
         #region Parameters
-        [Parameter(Mandatory = true, Position = 0)]
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = "Default")]
         [ValidateNotNull]
         public string[] Name { get; set; }
 
-        [Parameter(Mandatory = false)]
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = "InputObject")]
+        public PSBlueprintAssignment[] InputObject { get; set; }
+
+        [Parameter(Mandatory = false, ParameterSetName = "Default")]
         [ValidateNotNullOrEmpty]
         public string Subscription { get; set; }
         #endregion Parameters
@@ -38,23 +42,73 @@ namespace Microsoft.Azure.Commands.Blueprint.Cmdlets
         #region Cmdlet Overrides
         public override void ExecuteCmdlet()
         {
-            string subscription = Subscription ?? DefaultContext.Subscription.Id;
-
-            foreach (var name in Name)
+            switch (ParameterSetName)
             {
-                try
-                {
-                    Client.Assignments.Delete(subscription, name);
-                }
-                catch (Exception ex)
-                {
-                    WriteExceptionError(ex);
-                }
+                case "Default":
+                    HandleDefaultParameterSet();
+                    break;
+
+                case "InputObject":
+                    HandleInputObjectParameterSet();
+                    break;
             }
         }
         #endregion Cmdlet Overrides
 
         #region Private Methods
+        /// <summary>
+        /// Handle the "Default" parameter set.
+        /// </summary>
+        /// <remarks>
+        /// The "Default" parameter set removes assignments by name(s) either given
+        /// on the command line by the -Name parameter or input through the pipeline.
+        /// Optionally, a subscription ID can be provided through the -Subscription
+        /// parameter.
+        /// </remarks>
+        private void HandleDefaultParameterSet()
+        {
+            string subscription = Subscription ?? DefaultContext.Subscription.Id;
+
+            foreach (var name in Name)
+                PerformDelete(subscription, name);
+        }
+
+        /// <summary>
+        /// Handle the "InputObject" parameter set.
+        /// </summary>
+        /// <remarks>
+        /// The "InputObject" parameter set removes assignments referred to by
+        /// PSBlueprintAssignment object(s) either given on the command line by
+        /// the -InputObject parameter or input through the pipeline.
+        /// </remarks>
+        private void HandleInputObjectParameterSet()
+        {
+            foreach (var assignment in InputObject)
+                PerformDelete(assignment.SubscriptionId, assignment.Name);
+        }
+
+        /// <summary>
+        /// Perform the deletion of a blueprint assignment.
+        /// </summary>
+        /// <param name="subscription">The subscription ID for the assignment.</param>
+        /// <param name="name">The name of the assignment</param>
+        private void PerformDelete(string subscription, string name)
+        {
+            try
+            {
+                if (ShouldProcess(subscription + "/" + name))
+                {
+                    var assignment = Client.Assignments.Delete(subscription, name);
+
+                    if (assignment != null)
+                        WriteObject(PSBlueprintAssignment.FromAssignment(assignment, subscription));
+                }
+            }
+            catch (Exception ex)
+            {
+                WriteExceptionError(ex);
+            }
+        }
         #endregion Private Methods
     }
 }
